13e217f823362c6092c09d630350a08507ab5d11
dipesh <jaswanidipesh8@gmail.com>
2021-10-16 23:16:45 +0530

    zigzagTreeTravelsal added

---
+++ zigzagTravelsal.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+
+};
+
+void treeSpiral(Node *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    queue <Node *> q;
+    stack <int> s;
+    bool reverse = false;
+    q.push(root);
+
+    while(q.empty() == false)
+    {
+        int count = q.size();
+        for(int i = 0; i <count; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+
+            if(reverse)
+            {
+                s.push(curr->key);
+            }
+            else
+            {
+                cout<<curr->key<<" ";
+            }
+
+            if(curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            if(curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+
+        }
+
+            if(reverse)
+            {
+                while(s.empty() == false)
+                {
+                    cout<<s.top()<<" ";
+                    s.pop();
+                }
+            }
+
+            reverse = !reverse;
+            cout<<endl;
+    }
+}
+
+int main ()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->right->left = new Node(40);
+    root->right->right = new Node(50);
+
+    treeSpiral(root);
+
+}
